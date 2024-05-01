4a4471761bcc16dbd7ec9dfa9bd126965c78b736
Ariel <zech.programming@gmail.com>
2024-05-02 10:30:48 +1200

    Latest Activities, Total Count, constants

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,8 @@ import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
 
 
 export {
@@ -15,6 +17,8 @@ export {
   LatestActivitiesSkeleton,
   ProjectCardSkeleton,
   UpcomingEventsSkeleton,
+  DashboardTotalCountCard,
+  LatestActivities
 };
 
 /*
